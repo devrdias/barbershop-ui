dd3cc86b8e3d1956bcd609d944b3b07a20b3eea6
devrdias <devrdias@gmail.com>
2019-11-08 20:12:08 -0500

    fix path for history auth.sagas.js

--- src/store/modules/auth/auth.sagas.js
+++ src/store/modules/auth/auth.sagas.js
@@ -1,7 +1,7 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 import api from '~/services/api';
-import history from '~services/history';
+import history from '~/services/history';
 import { signInSuccess, signFailure } from './auth.actions';
 
 export function* signIn({ payload }) {
